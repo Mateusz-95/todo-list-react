b0826371185044ece41e86b5f761baf27796cd32
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2023-05-15 09:36:05 +0200

    spaces from 4 to 2 changed in Tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,32 +1,32 @@
 import "./style.css";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <ul className="list">
-        {tasks.map(task => (
-            <li key={task.id}
-                className={`list__item 
+  <ul className="list">
+    {tasks.map(task => (
+      <li key={task.id}
+        className={`list__item 
                 ${hideDone && task.done
-                        ? "list__item--hide"
-                        : ""}
+            ? "list__item--hide"
+            : ""}
                     `}
-            >
-                <button
-                    onClick={() => toggleTaskDone(task.id)}
-                    className="list__done list__buttons">
-                    {task.done ? "✅" : "🟩"}
-                </button >
-                <div className={`${task.done ? "list__content" : ""}`}>
-                    {task.content}
-                </div>
-                <button
-                    className="list__remove list__buttons"
-                    onClick={() => removeTask(task.id)}
-                >
-                    🗑️
-                </button>
-            </li>
-        ))}
-    </ul >
+      >
+        <button
+          onClick={() => toggleTaskDone(task.id)}
+          className="list__done list__buttons">
+          {task.done ? "✅" : "🟩"}
+        </button >
+        <div className={`${task.done ? "list__content" : ""}`}>
+          {task.content}
+        </div>
+        <button
+          className="list__remove list__buttons"
+          onClick={() => removeTask(task.id)}
+        >
+          🗑️
+        </button>
+      </li>
+    ))}
+  </ul >
 );
 
 export default Tasks;
