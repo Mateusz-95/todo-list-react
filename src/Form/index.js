5db142082aee153aa474e3fcc822f737d490203d
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2023-07-05 14:18:45 +0200

    focus on input after addNewTask

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Button, FormContainer, Input } from "./styled";
 
 const Form = ({ addNewTask }) => {
+  const inputRef = useRef(null);
+
   const [newTaskContent, setNewTaskContent] = useState("");
+
+  const focusOnInput = () => inputRef.current.focus();
 
   const onFormSubmit = (event) => {
     event.preventDefault();
     if (event.target.value !== "") {
       addNewTask(newTaskContent.trim());
       setNewTaskContent("");
+      focusOnInput();
     } else {
       return;
     }
@@ -17,6 +22,7 @@ const Form = ({ addNewTask }) => {
   return (
     <FormContainer onSubmit={onFormSubmit}>
       <Input
+        ref={inputRef}
         value={newTaskContent}
         placeholder="Co jest do zrobienia?"
         onChange={({ target }) => setNewTaskContent(target.value)}
